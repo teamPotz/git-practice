146841c56d51da9efb00b82c47e30e8998fb6604
suhyunlee <leesuhyun05505@gmail.com>
2023-11-01 16:46:13 +0900

    test 커밋

---
+++ Color.js
@@ -0,0 +1,25 @@
+//컬러 상수화
+
+const COLOR = {
+    WHITE : "#FFFFFF",
+    BLACK : "#000000",
+    POTZ_PINK_100 : "#FF7971",
+    POTZ_PINK_200 : "#FF4C41",
+    POTZ_PINK_300 : "#FF948E",
+    POTZ_PINK_400 : "#FFAAA5",
+    POTZ_PINK_500 : "#FFCECB",
+    POTZ_PINK_600 : "#FFF0EF",
+    POTZ_PINK_700 : "#FBF6F6",
+
+    GRAY_100 : "#373737",
+    GRAY_200 : "#808080",
+    GRAY_300 : "#A8A8A8",
+    GRAY_400 : "#CECECE",
+    GRAY_500 : "#EDEDED",
+
+    GREEN : "#00E741",
+    YELLOW : "#FFB571",
+    BLUE : "#0075FF"
+}
+
+export default COLOR;
