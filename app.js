4f6a90498822a0b434f3a48a7c036875c15771fe
uiyoung <suy2suy@gmail.com>
2023-11-02 11:19:35 +0900

    a경로 구현

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('hello from simple server :)');
 });
 
+app.get('/a', (req, res) => {
+  res.send('a 경로 들어왔을때 로직');
+});
+
 app.listen(port, () =>
   console.log('> Server is up and running on port : ' + port)
 );
