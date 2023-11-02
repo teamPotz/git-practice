c3666554da038d8e4eaee307757f92d5176c25f5
uiyoung <suy2suy@gmail.com>
2023-11-02 11:05:51 +0900

    add server

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+  res.send('hello from simple server :)');
+});
+
+app.listen(port, () =>
+  console.log('> Server is up and running on port : ' + port)
+);
