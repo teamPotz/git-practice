00edc7aa1dce62018c42fadf144797a75084a190
GISUNG KIM <nightfury805@gmail.com>
2023-11-02 11:19:53 +0900

    B기능

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const port = 5000;
 app.get('/', (req, res) => {
   res.send('hello from simple server :)');
 });
-
+app.get('/b', (req, res) => {
+  res.send('B기능');
+});
 app.listen(port, () =>
   console.log('> Server is up and running on port : ' + port)
 );
